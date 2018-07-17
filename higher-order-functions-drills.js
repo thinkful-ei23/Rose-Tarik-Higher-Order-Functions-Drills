91eef087fb6e6ca95372ef81b11283383ff5cb27
Rose Sorfa <rose.otter.2015@gmail.com>
2018-07-17 15:04:39 -0500

    added filter function

--- higher-order-functions-drills.js
+++ higher-order-functions-drills.js
@@ -18,3 +18,25 @@ function goodbye () {
 
 repeat(hello, 5);
 repeat(goodbye, 5);
+
+// DO NOT EDIT BETWEEN THESE LINES ----->
+// Return only names that begin with 'R'
+const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
+
+const filteredNames = filter(myNames, function(name) {
+    // This is a "predicate function" - it's a function that only returns a boolean
+    return name[0] === 'R';
+});
+
+console.log(filteredNames) // => ['Rich', 'Ray']
+// <---- DO NOT EDIT BETWEEN THESE LINES
+
+// TASK: DEFINE YOUR FILTER FUNCTION BELOW:
+function filter(arr, fn) {
+  const newArray = []; 
+  for (let i = 0; i < arr.length; i++) {
+    if (fn(arr[i]) === true) {
+      newArray.push(arr[i]);
+    }
+  } return newArray;
+} 
