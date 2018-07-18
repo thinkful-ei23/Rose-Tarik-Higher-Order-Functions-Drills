33aa3df1600171f1daa4cbb9143987da851dd09d
Tarik Desire <tprdesire@gmail.com>
2018-07-17 21:18:59 -0400

    Added reduce function

--- higher-order-functions-drills.js
+++ higher-order-functions-drills.js
@@ -1,5 +1,6 @@
 'use strict';
 
+//Functions as Arguments (1)
 function repeat (fn, n) {
   let i = 0;
   while (i < n) {
@@ -19,6 +20,9 @@ function goodbye () {
 repeat(hello, 5);
 repeat(goodbye, 5);
 
+
+
+//Functions as Arguments (2)
 // DO NOT EDIT BETWEEN THESE LINES ----->
 // Return only names that begin with 'R'
 const myNames = ['Rich', 'Joe', 'Bhaumik', 'Ray'];
@@ -42,13 +46,16 @@ function filter(arr, fn) {
 } 
 console.log(filter(myNames, name => name[0] === 'R')); 
 
+
+
+//Functions as Return Values
 function hazardWarningCreator (typeOfWarning) {
   let warningCounter = 0;
   return function(location) {
     warningCounter++;
     console.log(`DANGER! There is a ${typeOfWarning} hazard at ${location}!`);
     if (warningCounter === 1) {
-      `The ${typeOfWarning} hazard alert has triggered 1 time`;
+      console.log(`The ${typeOfWarning} hazard alert has triggered 1 time!`);
     } else {
       console.log(`The ${typeOfWarning} hazard alert has triggered ${warningCounter} times today!`);
     }
@@ -67,9 +74,37 @@ sharpTurn('Centinela Ave and Olympic Blvd');
 accidentAhead('Main St and Pacific Ave');
 accidentAhead('Centinela Ave and Olympic Blvd');
 
+
+
+//forEach, filter, and map
 const turtleMoves = [[0, 0], [0, 5], [-1, -3], [-3, 1], [2, -4], [3, 2]]; 
-const noNegs = turtleMoves.filter(innerArray => innerArray[0] > 0 && innerArray[1] > 0);
-const addSteps = turtleMoves.map(innerArray => innerArray[0] + innerArray[1]); 
+const noNegs = turtleMoves.filter(innerArray => innerArray[0] >= 0 && innerArray[1] >= 0);
+const addSteps = noNegs.map(innerArray => innerArray[0] + innerArray[1]); 
 addSteps.forEach(function(num) {
   console.log(num);
-}); 
+});
+
+
+
+//Reduce
+const sentence =  'noggin oreo the moon time tele steed his tent apollo her lives though shoo tofu budapest';
+
+function decodedSentence (decodeSentence) {
+  const sentArr = decodeSentence.split(' ');
+  let mapArr = [];
+  sentArr.forEach(function(word) {
+    if (word.length === 3) {
+      mapArr.push(' ');
+    } else {
+      mapArr.push(word[word.length - 1].toUpperCase());
+    }
+  });
+  const reducer = function (accumulator, currentValue) {
+    return accumulator.concat(currentValue);
+  };
+  let decodedWord = mapArr.reduce(reducer);
+  return decodedWord;
+}
+
+console.log(decodedSentence(sentence));
+
